cc3b8c94cceddec73fb709d597d41b085e585b0a
bobochang <m13726750698_2@163.com>
2023-08-03 11:33:53 +0800

    first init

---
+++ src/main/java/com/bobochang/bobochangoj/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.bobochang.bobochangoj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bobochang.bobochangoj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/libobochang">程序员鱼皮</a>
+ * @from <a href="https://bobochang.icu">编程导航知识星球</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
